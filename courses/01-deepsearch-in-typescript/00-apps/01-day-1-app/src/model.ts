6c88849e95916d88b45dd50aeac9a43fb9858dc1
Barbulescu Alexandru <alexandru.barbulescu@externals.adidas.com>
2025-07-24 13:13:33 +0300

    chore: remove second model declaration

--- courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/src/model.ts
+++ courses/01-deepsearch-in-typescript/00-apps/01-day-1-app/src/model.ts
@@ -1,9 +1,3 @@
 import { google } from "@ai-sdk/google";
 
-// OK!
-export const model = google("gemini-2.0-flash-001", {
-  cachedContent: "todo",
-});
-
-// Error!
-export const model2 = google("gemini-2.0-flash-001", {});
+export const model = google("gemini-2.0-flash-001");
